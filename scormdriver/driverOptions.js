c5714c0edfd75c5dd5c6a6f3f8c927e6fc920c2d
Abdallah Khemais <45246688+nevermind78@users.noreply.github.com>
2022-09-22 00:13:31 +0100

    articulate

---
+++ scormdriver/driverOptions.js
@@ -0,0 +1,8 @@
+function loadDriverOptions (scope) {
+  scope.APPID = "WQBM2ARBZR";
+  scope.CLOUDURL = null;
+  scope.USE_STRICT_SUSPEND_DATA_LIMITS = false;
+  scope.FORCED_COMMIT_TIME = 60000;
+  scope.strLMSStandard = "SCORM";
+  scope.REVIEW_MODE_IS_READ_ONLY = true;
+}
